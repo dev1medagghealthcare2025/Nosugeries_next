36f855ff079e517fb91b215aca5210cc97921391
dev1medagghealthcare2025 <dev1.medagghealthcare@gmail.com>
2026-04-01 10:47:34 +0530

    Tag_Manager_add_01-04-26

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,6 +18,9 @@ export const metadata = {
     template: '%s | NoSurgeries',
   },
   description: 'NoSurgeries by Medagg Healthcare — Expert non-surgical Interventional Radiology treatments across India. Book a consultation today.',
+  verification: {
+    google: 'AzZ_GNWhvqpT8rGNQYHamI2I5LYTTVGoDj35ExCQnjI',
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -25,7 +29,34 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KS9VVWMV');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
+      <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KS9VVWMV"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        {children}
+      </body>
     </html>
   );
 }
