abfff4209eb0a201c4de9ac2e410dadaee51c276
dev1medagghealthcare2025 <dev1.medagghealthcare@gmail.com>
2026-04-01 12:46:09 +0530

    Sitemap_robots_added_01-04-26

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://no-surgeries.com/sitemap.xml',
+  };
+}
---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://no-surgeries.com/',
+      lastModified: '2026-04-01T05:31:15+00:00',
+      priority: 1.0,
+    },
+    {
+      url: 'https://no-surgeries.com/about',
+      lastModified: '2026-04-01T05:31:15+00:00',
+      priority: 0.8,
+    },
+    {
+      url: 'https://no-surgeries.com/varicocele-embolization-vc',
+      lastModified: '2026-04-01T05:31:15+00:00',
+      priority: 0.8,
+    },
+    {
+      url: 'https://no-surgeries.com/fallopian-tube-recanalization-ftr',
+      lastModified: '2026-04-01T05:31:15+00:00',
+      priority: 0.8,
+    },
+  ];
+}
